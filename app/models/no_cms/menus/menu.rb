5641e5a3cdf4cc154efc98e960ddd90e75d8e26d
David J. Brenes <david.brenes@simplelogica.net>
2014-03-25 14:15:45 +0100

    Testing relationship between menu and menu items

--- app/models/no_cms/menus/menu.rb
+++ app/models/no_cms/menus/menu.rb
@@ -2,6 +2,8 @@ module NoCms::Menus
   class Menu < ActiveRecord::Base
     translates :name
 
+    has_many :menu_items
+
     validates :name, :uid, presence: true
     validates :uid, uniqueness: true
   end
