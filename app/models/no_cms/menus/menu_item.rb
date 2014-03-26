889bf525d0780596c2f0d616382fce1b7844d37e
David J. Brenes <david.brenes@simplelogica.net>
2014-03-26 11:52:33 +0100

    Detecting active menu items for actions

--- app/models/no_cms/menus/menu_item.rb
+++ app/models/no_cms/menus/menu_item.rb
@@ -12,10 +12,14 @@ module NoCms::Menus
 
     scope :active_for, ->(options) do
       object = options[:object]
-      active_for_object(object) unless options[:object].nil?
+      return active_for_object(object) unless options[:object].nil?
+      action = options[:action]
+      active_for_action(action) unless options[:action].nil?
     end
 
     scope :active_for_object, ->(object) { where menuable_type: object.class, menuable_id: object.id }
+
+    scope :active_for_action, ->(action) { where menu_action: action }
 
   end
 end
--- spec/models/no_cms/menus/menu_item_spec.rb
+++ spec/models/no_cms/menus/menu_item_spec.rb
@@ -8,21 +8,33 @@ describe NoCms::Menus::Menu do
 
   context "detecting active menu items" do
 
+    let(:action_menu_item) { create :no_cms_menus_menu_item, menu_action: 'pages#show' }
+    let(:page_menu_item) { create :no_cms_menus_menu_item, menuable: page }
+    let(:page) { create :page }
+    let(:other_menu_items) { create_list :no_cms_menus_menu_item, 2 }
+    let(:other_page_menu_item) { create :no_cms_menus_menu_item, menuable: other_page }
+    let(:other_page) { create :page }
+
+
+    before { action_menu_item && page_menu_item && other_menu_items && other_page_menu_item }
+
+
     context "when attached to an object" do
-
-      let(:page_menu_item) { create :no_cms_menus_menu_item, menuable: page }
-      let(:page) { create :page }
-      let(:other_menu_items) { create_list :no_cms_menus_menu_item, 2 }
-      let(:other_page_menu_item) { create :no_cms_menus_menu_item, menuable: other_page }
-      let(:other_page) { create :page }
-
-
-      before { page_menu_item && other_menu_items && other_page_menu_item }
 
       subject { page_menu_item }
 
       it "should detect only those attached to that object" do
         expect(NoCms::Menus::MenuItem.active_for(object: page)).to eq [page_menu_item]
+      end
+
+    end
+
+    context "when attached to an action" do
+
+      subject { action_menu_item }
+
+      it "should detect only those attached to that object" do
+        expect(NoCms::Menus::MenuItem.active_for(action: 'pages#show')).to eq [action_menu_item]
       end
 
     end
