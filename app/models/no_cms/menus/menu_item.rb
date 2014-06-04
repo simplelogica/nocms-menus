032e20cea40f1211b895e59b88f2f579f111487c
David J. Brenes <david.brenes@simplelogica.net>
2014-06-04 12:27:43 +0200

    Modified the url_for method so we can handle controller/actions from different engines

--- app/models/no_cms/menus/menu_item.rb
+++ app/models/no_cms/menus/menu_item.rb
@@ -64,7 +64,8 @@ module NoCms::Menus
           menuable.respond_to?(:path) ? menuable.path : menuable
         when !menu_kind[:action].nil?
           controller, action = menu_action.split('#')
-          { controller: controller, action: action }
+          # When we are inside an engine we need to prepend '/' to the controller, or else it would search the controller in the current engine
+          { controller: "/#{controller}", action: action }
         else
           external_url
       end
