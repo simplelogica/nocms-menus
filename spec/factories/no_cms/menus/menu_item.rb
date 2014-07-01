02f48a926dac88dfe4d8307bf123ce509983d4d5
David J. Brenes <david.brenes@simplelogica.net>
2014-07-01 16:01:19 +0200

    Menu items now need some menu

--- spec/factories/no_cms/menus/menu_item.rb
+++ spec/factories/no_cms/menus/menu_item.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :no_cms_menus_menu_item, class: NoCms::Menus::MenuItem do
     name { Faker::Lorem.sentence }
     kind { 'fixed_url' }
+    menu { FactoryGirl.create :no_cms_menus_menu }
   end
 end
